0c1624ffc8305d07d2ed19d6ea355d641926e1b0
teameftake <92701246+teameftake@users.noreply.github.com>
2021-10-29 12:14:57 +0900

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+class FizzBuzz
+{
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=16; i++)
+		{
+			if(i%3==0 && i%5 ==0){
+				System.out.print("FizzBuzz");
+			}
+			if(i%3==0){
+				System.out.print("Fizz");
+			}
+			if(i%5==0){
+				System.out.print("Buzz");
+			}
+			else{
+				System.out.print(i);
+			}
+		}
+		System.out.print("");
+	}
+}
